5b985fa15ec9a3bfeffe99dc19d7306c6f99f557
rizfirman <rizfirman@gmail.com>
2021-04-01 11:42:36 +0700

    create a login card

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/scss/_global.scss'
+
 
 ReactDOM.render(
   <React.StrictMode>
